ac9d893ec192cc7c788cca9cacd18db5bf838ad5
aditya shukla <adityashukla74@gmail.com>
2018-01-08 21:11:26 +0530

    generating hash properly for the first block

--- blockchain.js
+++ blockchain.js
@@ -9,12 +9,12 @@ var SHA256 = require("crypto-js/sha256");
  * @class Block
  */
 class Block {
-    constructor(index, timestamp, data, previousHash='') {
+    constructor(index, timestamp, data, previousHash = '') {
         this.index = index;
         this.timestamp = timestamp;
         this.data = data;
         this.previousHash = previousHash;
-        this.hash = this.calculateHash; //calculate hash of block
+        this.hash = this.calculateHash(); //calculate hash of block
     }
     /**
      * Calculate hash function of the block by taking properties of the block in constructor
@@ -31,10 +31,11 @@ class Block {
  * 
  * @class Blockchain
  */
-class Blockchain {
-    constructor() { //initialize blockchain
+class Blockchain{
+    constructor() {
         this.chain = [this.createGenesBlock()];
     }
+
     /**
      * First block of blockchain will return index, date, some data "genesis block and previous hash"
      * @returns 
@@ -43,17 +44,22 @@ class Blockchain {
     createGenesBlock() {
         return new Block(0, "04/01/2018", "genesis block", "0");
     }
-/**
- * returns latest block in the chain, last element 
- * @returns     
- * @memberof Blockchain
- */
-getLatestBlock() {
-        return this.chain(this.chain.length - 1);
+
+    /**
+     * returns latest block in the chain, last element 
+     * @returns     
+     * @memberof Blockchain
+     */
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
     }
 
-    addLock(newBlock) {
-        newBlock.previousHash = this.getLatestBlock.hash;
+    /**
+     * @param {any} newBlock 
+     * @memberof Blockchain
+     */
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
         newBlock.hash = newBlock.calculateHash(); //recalculate hash for the new changed block
         this.chain.push(newBlock);
     }
@@ -62,8 +68,8 @@ getLatestBlock() {
 // Test the block
 let myCoin = new Blockchain();
 // add few blocks
-myCoin.addLock(new Block(1, "05/01/2018", { amount: 10 }));
-myCoin.addLock(new Block(2, "07/01/2018", { amount: 100 }));
+myCoin.addBlock(new Block(1, "05/01/2018", { amount: 10 }));
+myCoin.addBlock(new Block(2, "07/01/2018", { amount: 100 }));
 
 console.log(JSON.stringify(myCoin, null, 4));
 
